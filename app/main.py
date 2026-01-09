39a09a09c96e553f9313eb9ea313f1d8e129df6f
KrzysiekJa <kjmj77@gmail.com>
2026-01-10 00:36:08 +0100

    Added basic backend in `main.py`.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from ai-transcript-summarizer!")
+from typing import Any
 
+from fastapi import FastAPI, APIRouter, Depends, Request, status
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+from app import deps
+
+
+app: FastAPI = FastAPI(title="AI Transcript Summarizer", version="1.0.0")
+api_router = APIRouter()
+
+
+@api_router.get("/", status_code=status.HTTP_200_OK)
+async def root(request: Request, db: AsyncSession = Depends(deps.get_db)) -> Any:
+    return {"status": "ok"}
+
+
+app.include_router(api_router)
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+
+    uvicorn.run(
+        "app.main:app", host="0.0.0.0", port=8005, log_level="debug", reload=True
+    )
